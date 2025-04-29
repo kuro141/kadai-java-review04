0d094305bc378ebf05e66329d549b0603a8bb670
kuro141 <karasu141.shinon@icloud.com>
2025-04-29 15:23:07 +0900

    Javaプログラミング学習終了

---
+++ src/UseVariable.java
@@ -0,0 +1,14 @@
+
+public class UseVariable {
+
+	public static void main(String[] args) {
+		int [] score = new int[5];
+		score[0] = 84;
+		score[1] = 77;
+		score[2] = 59;
+		score[3] = 91;
+		score[4] = 87;
+
+	}
+
+}
