3eb2a40c4fb3b4d3499532b7eba8adc2f0898ffd
kuro141 <karasu141.shinon@icloud.com>
2025-05-04 19:20:30 +0900

    レッスン4FizzBazz問題　再提出

--- src/Review02.java
+++ src/Review02.java
@@ -1,23 +1,18 @@
-
 public class Review02 {
 
 	public static void main(String[] args) {
 		for (int i = 1; i <= 100; i++) {
-			if (i % 3 == 0 && i%5 == 0) {
+			if (i % 3 == 0 && i % 5 == 0) {
 				System.out.println("FizzBuzz!");
-					continue;
 			}
-            	if (i % 3 == 0) {
-            		System.out.println("Fizz!");
-        	    		continue;
-            	}
-					if (i % 5 == 0) {
-            			System.out.println("Buzz!");
-            				continue;
-					}
-	    				else {
-        	    					System.out.println(i);
-	    				}
+			else if (i % 3 == 0) {
+				System.out.println("Fizz!");
+			}
+			else if (i % 5 == 0) {
+				System.out.println("Buzz!");
+			} else {
+				System.out.println(i);
+			}
 		}
 	}
 }
